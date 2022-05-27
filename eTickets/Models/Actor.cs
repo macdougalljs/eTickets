1e6295b7c9440e598a143b85df0334a6233e3e70
Jeremy MacDougall <macdojer@outlook.com>
2022-05-27 14:56:21 -0400

    Added the Actors View

--- eTickets/Models/Actor.cs
+++ eTickets/Models/Actor.cs
@@ -10,15 +10,17 @@ namespace eTickets.Models
         {
             get; set;
         }
+        [Display(Name ="Profile Picture URL")]
         public string ProfilePictureURL
         {
             get; set;
         }
+        [Display(Name = "Full Name")]
         public string FullName
         {
             get; set;
         }
-
+        [Display(Name = "Bio")]
         public string Bio
         {
             get; set;
