6f876776e3e2479c75b7fa6d0bec318088d454eb
Jeremy MacDougall <macdojer@outlook.com>
2022-05-28 11:13:49 -0400

    Add List Of Cinemas View

--- eTickets/Models/Cinema.cs
+++ eTickets/Models/Cinema.cs
@@ -10,15 +10,18 @@ namespace eTickets.Models
         {
             get; set;
         }
+        
+        [Display(Name = "Cinema Logo")]
         public string Logo
         {
             get; set;
         }
+        [Display(Name = "Cinema Name")]
         public string Name
         {
             get; set;
         }
-
+        [Display(Name = "Description")]
         public string Description
         {
             get; set;
