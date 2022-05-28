b09d7217801288f554dd02087f48bc10685ac0cc
Jeremy MacDougall <macdojer@outlook.com>
2022-05-28 11:04:12 -0400

    Add Producers View

--- eTickets/Models/Actor.cs
+++ eTickets/Models/Actor.cs
@@ -10,7 +10,7 @@ namespace eTickets.Models
         {
             get; set;
         }
-        [Display(Name ="Profile Picture URL")]
+        [Display(Name ="Profile Picture")]
         public string ProfilePictureURL
         {
             get; set;
--- eTickets/Models/Producer.cs
+++ eTickets/Models/Producer.cs
@@ -10,15 +10,17 @@ namespace eTickets.Models
         {
             get; set;
         }
+        [Display(Name = "Profile Picture")]
         public string ProfilePictureURL
         {
             get; set;
         }
+        [Display(Name = "Full Name")]
         public string FullName
         {
             get; set;
         }
-
+        [Display(Name = "Biography")]
         public string Bio
         {
             get; set;
