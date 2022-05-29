2671a26c3ceb0e385d94f6e916cc4b891ef06da1
Jeremy MacDougall <macdojer@outlook.com>
2022-05-29 12:03:36 -0400

    Adding your first service

--- eTickets/Data/Services/IActorsService.cs
+++ eTickets/Data/Services/IActorsService.cs
@@ -1,6 +1,7 @@
 ﻿using eTickets.Models;
 using System.Collections;
 using System.Collections.Generic;
+using System.Threading.Tasks;
 
 namespace eTickets.Data.Services
 {
@@ -8,14 +9,13 @@ namespace eTickets.Data.Services
     {
         // we are just going to device the method signatures - an interface is just a contract
 
-        IEnumerable<Actor> GetAll();
+        Task<IEnumerable<Actor>> GetAll();
 
         Actor GetById(int id);
-
+      
         void Add(Actor actor);
 
         Actor Update(int id, Actor newActor);
-
         void Delete(int id);
     }
 }
