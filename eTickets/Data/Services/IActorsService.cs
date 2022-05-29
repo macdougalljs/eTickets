f9387dda95d88a81f46fb08274b47fabb57df0f6
Jeremy MacDougall <macdojer@outlook.com>
2022-05-29 11:48:44 -0400

    Adding IActors interface

---
+++ eTickets/Data/Services/IActorsService.cs
@@ -0,0 +1,21 @@
+﻿using eTickets.Models;
+using System.Collections;
+using System.Collections.Generic;
+
+namespace eTickets.Data.Services
+{
+    public interface IActorsService
+    {
+        // we are just going to device the method signatures - an interface is just a contract
+
+        IEnumerable<Actor> GetAll();
+
+        Actor GetById(int id);
+
+        void Add(Actor actor);
+
+        Actor Update(int id, Actor newActor);
+
+        void Delete(int id);
+    }
+}
