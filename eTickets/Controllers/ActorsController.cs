1d481b80df57d1a6a123610953eddec617ab1cfc
Jeremy MacDougall <macdojer@outlook.com>
2022-05-29 13:00:12 -0400

    Designing your first form - Create Actor

--- eTickets/Controllers/ActorsController.cs
+++ eTickets/Controllers/ActorsController.cs
@@ -9,16 +9,23 @@ namespace eTickets.Controllers
     public class ActorsController : Controller
     {
         private readonly IActorsService _service;
-      
+
         public ActorsController(IActorsService service)
         {
-            _service = service;  
+            _service = service;
         }
 
         public async Task<IActionResult> Index()
         {
             var data = await _service.GetAll();
             return View(data);
+        }
+
+        //Get: Actors/Create
+        public IActionResult Create()
+        {
+            return View();
+
         }
     }
 }
