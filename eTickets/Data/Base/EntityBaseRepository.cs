e07f2f67ab2d49ebf46f561ac412e28935eb2d6a
Jeremy MacDougall <macdojer@outlook.com>
2022-06-04 14:43:20 -0400

    UpdateAsync() and DeleteAsync() in base repository

--- eTickets/Data/Base/EntityBaseRepository.cs
+++ eTickets/Data/Base/EntityBaseRepository.cs
@@ -14,22 +14,24 @@ namespace eTickets.Data.Base
         }
         public async Task AddAsync(T entity) => await _context.Set<T>().AddAsync(entity);
 
-
-        public Task DeleteAsync(int id)
+        public async Task DeleteAsync(int id)
         {
-            throw new System.NotImplementedException();
+            var result = await _context.Set<T>().FirstOrDefaultAsync(n => n.Id == id);
+            _context.Set<T>().Remove(result);
+            await _context.SaveChangesAsync();
         }
 
         public async Task<IEnumerable<T>> GetAllAsync() => await _context.Set<T>().ToListAsync();
-     
 
         public async Task<T> GetByIdAsync(int id) => await _context.Set<T>().FirstOrDefaultAsync(n => n.Id == id);
 
-
-
-        public Task<T> UpdateAsync(int id, T entity)
+        public async Task<T> UpdateAsync(int id, T entity)
         {
-            throw new System.NotImplementedException();
+            _context.Update(entity);
+            await _context.SaveChangesAsync();
+            return entity;
+
         }
+   
     }
 }
