8683cccec40ad20a171efffbe24e22c2b3586485
Jeremy MacDougall <macdojer@outlook.com>
2022-06-06 15:49:08 -0400

    Get cinema details

--- eTickets/Controllers/CinemasController.cs
+++ eTickets/Controllers/CinemasController.cs
@@ -40,5 +40,16 @@ namespace eTickets.Controllers
             return RedirectToAction(nameof(Index));
         }
 
+        //GET: Cinema/Details/1
+
+        public async Task<IActionResult> Details(int id)
+        {
+            var cinemaDetails = await _service.GetByIdAsync(id);
+            if (cinemaDetails == null)
+                return View("NotFound");
+            return View(cinemaDetails);
+        }
+           
+
     }
 }
