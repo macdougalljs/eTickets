30067dc96857e07ab3756e960118cf748fed557e
Jeremy MacDougall <macdojer@outlook.com>
2022-06-08 12:36:22 -0400

    ViewComponents

---
+++ eTickets/Data/ViewComponents/ShoppingCartSummary.cs
@@ -0,0 +1,25 @@
+﻿using eTickets.Data.Cart;
+using Microsoft.AspNetCore.Mvc;
+
+namespace eTickets.Data.ViewComponents
+{
+
+    public class ShoppingCartSummary : ViewComponent
+    {
+        //inject the shopping cart
+
+        private readonly ShoppingCart _shoppingcart;
+
+        public ShoppingCartSummary(ShoppingCart shoppingcart)
+        {
+            _shoppingcart = shoppingcart;
+        }
+
+        public IViewComponentResult Invoke()
+        {
+            var items = _shoppingcart.GetShoppingCartItems();
+            return View(items.Count);
+        }
+
+    }
+}
