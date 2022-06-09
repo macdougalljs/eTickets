737f60b84e57314b1cdddb8a5fa34fdd7663ff27
Jeremy MacDougall <macdojer@outlook.com>
2022-06-09 12:09:22 -0400

    Signing up users

--- eTickets/Controllers/AccountController.cs
+++ eTickets/Controllers/AccountController.cs
@@ -1,15 +1,18 @@
 ﻿using eTickets.Data;
+using eTickets.Data.Static;
 using eTickets.Data.ViewModels;
 using eTickets.Models;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
 using System.Threading.Tasks;
 
 namespace eTickets.Controllers
 {
     public class AccountController : Controller
     {
-        // inject user manager, signin manager, and dbcontext
         private readonly UserManager<ApplicationUser> _userManager;
         private readonly SignInManager<ApplicationUser> _signInManager;
         private readonly AppDbContext _context;
@@ -26,7 +29,8 @@ namespace eTickets.Controllers
         [HttpPost]
         public async Task<IActionResult> Login(LoginVM loginVM)
         {
-            if (!ModelState.IsValid) return View(loginVM);
+            if (!ModelState.IsValid)
+                return View(loginVM);
 
             var user = await _userManager.FindByEmailAsync(loginVM.EmailAddress);
             if (user != null)
@@ -40,16 +44,43 @@ namespace eTickets.Controllers
                         return RedirectToAction("Index", "Movies");
                     }
                 }
-                TempData["Error"] = "Wrong credentials. Please try again!";
+                TempData["Error"] = "Wrong credentials. Please, try again!";
                 return View(loginVM);
-
             }
 
-            TempData["Error"] = "Wrong credentials. Please try again!";
+            TempData["Error"] = "Wrong credentials. Please, try again!";
             return View(loginVM);
         }
 
+
         public IActionResult Register() => View(new RegisterVM());
+
+        [HttpPost]
+        public async Task<IActionResult> Register(RegisterVM registerVM)
+        {
+            if (!ModelState.IsValid)
+                return View(registerVM);
+
+            var user = await _userManager.FindByEmailAsync(registerVM.EmailAddress);
+            if (user != null)
+            {
+                TempData["Error"] = "This email address is already in use";
+                return View(registerVM);
+            }
+
+            var newUser = new ApplicationUser()
+            {
+                FullName = registerVM.FullName,
+                Email = registerVM.EmailAddress,
+                UserName = registerVM.EmailAddress
+            };
+            var newUserResponse = await _userManager.CreateAsync(newUser, registerVM.Password);
+
+            if (newUserResponse.Succeeded)
+                await _userManager.AddToRoleAsync(newUser, UserRoles.User);
+
+            return View("RegisterCompleted");
+        }
 
     }
 }
